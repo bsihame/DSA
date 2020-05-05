5ff0bc632b20ffd99177a518bef47293093ef6ce
bsihame <sihamebazi@pursuit.org>
2020-05-05 05:14:24 -0400

    add iterative code for factorial

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,47 +1,62 @@
 // Count down to zero
-console.log("count down to zero Iterative Loops method");
-const countDownToZeroIterative = (num) => {
-  for (let i = num; i > 0; i--) {
-    console.log(i)
-  }
-}
-console.log(countDownToZeroIterative(10))
+// console.log("count down to zero Iterative Loops method");
+// const countDownToZeroIterative = (num) => {
+//   for (let i = num; i > 0; i--) {
+//     console.log(i)
+//   }
+// }
+// console.log(countDownToZeroIterative(10))
 
-console.log("count down to zero Recursive method");
+// console.log("count down to zero Recursive method");
 
-const countDownToZero = (num) => {
-  // Let set first set a base case
-  if (num <= 0) return;
-  console.log(num);
-  countDownToZero(num-1)
-}
- console.log(countDownToZero(10))
+// const countDownToZero = (num) => {
+//   // Let set first set a base case
+//   if (num <= 0) return;
+//   console.log(num);
+//   countDownToZero(num-1)
+// }
+//  console.log(countDownToZero(10))
+// console.log("-------------------------------")
+
+// console.log("count up by 2 Iterative method");
+// const countUpIterative = (start, to) => {
+//   for (let i =start; i <= to; i =i+2) {
+//     console.log(i)
+//   }
+// }
+// console.log(countUpIterative(0, 10));
+
+
+// console.log("count up by 2 Recursive method")
+
+// const countUp = (min, max) => {
+//   if (max < min) return;
+//   console.log(min);
+//   countUp(min + 2, max)
+// }
+// console.log(countUp(0, 10))
+
 console.log("-------------------------------")
 
-console.log("count up by 2 Iterative method");
-const countUpIterative = (start, to) => {
-  for (let i =start; i <= to; i =i+2) {
-    console.log(i)
+console.log("Factorial Iterative method");
+
+const factorialIterative = (num) => {
+  let result = num;
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
   }
+  return result
 }
-console.log(countUpIterative(0, 10));
+console.log(factorialIterative(5));
 
+console.log("Factorial Recursive method");
 
-console.log("count up by 2 Recursive method")
-
-const countUp = (min, max) => {
-  if (max < min) return;
-  console.log(min);
-  countUp(min + 2, max)
+const factorialRec = (num) => {
+  if (num < 0) return ("No negatives!");
+  if (num <= 1) return 1;
+  return factorialRec(num - 1)* num
 }
-console.log(countUp(0, 10))
-
-// console.log("Factorial");
-
-// const factorialRec = (num) => {
-//   if (num < 0) return ("No negatives!");
-//   if (num <= 1) return 1;
-//   return factorialRec(num - 1)* num
-// }
-// console.log(factorialRec(5))
+console.log(factorialRec(5))
 
